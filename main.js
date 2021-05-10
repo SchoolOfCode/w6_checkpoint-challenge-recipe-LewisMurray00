2795f19a0ca570e7a5bcd5e3e21e1d4c3a80fe4f
Lewis Murray <lewisdmurray00@gmail.com>
2021-05-10 20:13:06 +0100

    Set up the link, works in console

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const appId = "cc3547a6";
+const appKey = "5f291e4e74b80618f864b4ca0c647341";
+const requestURL = `https://api.edamam.com/search?q=kale&app_id=${appId}&app_key=${appKey}`;
+const recipeLink = document.querySelector("#recipe-label");
+
 let foodToSearch = null;
 
 function handleRecipeClick() {
@@ -11,4 +16,9 @@ function handleFoodChange() {
 async function fetchRecipe(food) {
   //--- write your code below ---
   //--- write your code above ---
-}
+  const response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=cc3547a6&app_key=5f291e4e74b80618f864b4ca0c647341`);
+  const recipe = await response.json();
+  //console.log(recipe);
+  recipeLink.innerHTML = recipe; 
+
+};
